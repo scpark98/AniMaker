5d950633948cb88811739dfd18747785c327bd14
scpark98 <scpark98@gmail.com>
2026-02-27 17:53:48 +0900

    .

---
+++ resource.h
@@ -0,0 +1,55 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++에서 생성한 포함 파일입니다.
+// AniMaker.rc에서 사용되고 있습니다.
+//
+#define IDD_ABOUTBOX                    100
+#define IDP_OLE_INIT_FAILED             100
+#define IDR_MAINFRAME                   128
+#define IDR_AniMakerTYPE                130
+#define ID_WINDOW_MANAGER               131
+#define IDD_PREVIEW                     310
+#define IDR_MENU_FRAME_CONTEXT          312
+#define IDR_MENU_CONTEXT                313
+#define IDD_FRAME_PROPERTY              313
+#define IDC_EDIT_DELAY                  1000
+#define ID_32771                        32771
+#define ID_MENU_PREVIEW                 32772
+#define ID_FRAME_PASTE                  32773
+#define ID_PASTE_INTOSELECTEDFRAME      32774
+#define ID_MENU_PASTE_INTO_SELECTED_FRAME 32775
+#define ID_MENU_PASTE_BEFORE_CURRENT_FRAME 32776
+#define ID_MENU_PASTE_AFTER_CURRENT_FRAME 32777
+#define ID_PASTE_CUT                    32778
+#define ID_PASTE_COPY                   32779
+#define ID_FRAME_DELETE                 32780
+#define ID_FRAME_DUPLICATESELECTED      32781
+#define ID_FRAME_INSERTFRAME            32782
+#define ID_INSERTFRAME_FROMFILE         32783
+#define ID_INSERTFRAME_EMPTY            32784
+#define ID_FRAME_FRAMEPROPERTY          32785
+#define ID_FRAME_SAVEFRAMEAS            32786
+#define ID_MENU_CUT                     32787
+#define ID_MENU_COPY                    32788
+#define ID_MENU_DELETE                  32789
+#define ID_MENU_DUPLICATE_SELECTED      32790
+#define ID_MENU_INSERT_FRAME_FROM_FILE  32791
+#define ID_MENU_INSERT_FRAME_EMPTY      32792
+#define ID_MENU_FRAME_PROPERTY          32793
+#define ID_MENU_SAVE_FRAME_AS           32794
+#define ID_CONTEXT_GOTOFRAME            32795
+#define ID_CONTEXT_VIEWANIMATION        32796
+#define ID_MENU_GO_TO_FRAME             32797
+#define ID_MENU_VIEW_ANIMATION          32798
+#define ID_MENU_SAVE_IMAGE_AS           32799
+#define ID_MENU_SAVE_AS                 32800
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        315
+#define _APS_NEXT_COMMAND_VALUE         32801
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           310
+#endif
+#endif
